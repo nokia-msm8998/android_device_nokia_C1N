b37057efb1437c41aa927cd8de17bae168e02dfc
Alexander Martinz <amartinz@shiftphones.com>
2026-03-31 06:49:03 +0000

    C1N: Use libhidlbase-v32 for fingerprint libs
    Change-Id: Iaef8ede57250aa171f9863e7e250c604ef241334

--- extract-files.py
+++ extract-files.py
@@ -36,6 +36,8 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('__aeabi_memcpy')
         .clear_symbol_version('__aeabi_memset')
         .clear_symbol_version('__gnu_Unwind_Find_exidx'),
+    ('vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so', 'vendor/lib64/libvendor.goodix.hardware.fingerprintextension@1.0.so'): blob_fixup()
+        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
